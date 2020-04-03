f6f5a8db11bb36f3131bb58565b04fa861e85c31
izzy <isratjahanmou.official@gmail.com>
2020-04-03 06:49:59 +0600

    initial commit

---
+++ css/index.php
@@ -0,0 +1,360 @@
+<!doctype html>
+<html lang="en-US">
+<head>
+<title>Auto</title>
+<meta charset="utf-8">
+
+<!-- Meta -->
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+<meta name="author" content="">
+<meta name="robots" content="" />
+
+<!-- Responsive View  -->
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<!-- Favicon -->
+<link rel="shortcut icon" href="../images/favicon.ico">
+
+<!-- Google fonts-->
+<link href='https://fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,400italic,500,700,500italic,700italic,900,900italic' rel='stylesheet' type='text/css'>
+
+<link href='https://fonts.googleapis.com/css?family=Aldrich' rel='stylesheet' type='text/css'>
+<!--[if lt IE 9]>
+<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+<!-- FONT awesome STYLES -->
+<link rel="stylesheet" href="font-awesome.min.css" type="text/css" />
+
+<!-- BOOTSTRAP STYLES -->
+<link rel="stylesheet" href="bootstrap.css" type="text/css" />
+
+<!-- uikit  -->
+<link rel="stylesheet" href="uikit.css" type="text/css" />
+
+<!--accordion.....-->
+<link rel="stylesheet" href="main.css">
+
+<!--slider-->
+<link rel="stylesheet" media="all" href="jquery.bxslider.css" type="text/css">
+
+<!-- Menu CSS -->
+<link rel="stylesheet" type="text/css" href="menu.css" media="all" />
+
+<!-- CSS STYLES -->
+<link rel="stylesheet" href="style.css" type="text/css" />
+<link rel="stylesheet" href="template.css" type="text/css" />
+
+<!-- Responsive Devices Styles -->
+<link rel="stylesheet" media="screen" href="responsive-leyouts.css" type="text/css" />
+
+</head>
+
+<body>
+	<header> 
+   		<div class="container">
+        	<div class="logo-area">
+            	<a href="../."><img src="../images/logo.png" alt="logo"></a>	
+            </div>	
+            <!--logo-area ends....-->
+            <div class="header-right">
+            	<ul class="top">
+            		<li><i class="fa fa-envelope" aria-hidden="true"></i>Info@autofuture.nl</li>
+            		<li><i class="fa fa-mobile" aria-hidden="true"></i>06-85097273</li>
+            	</ul>	
+                <ul class="bottom">
+            		<li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+            		<li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+                    <li><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+            		<li><a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+            	</ul>
+            </div>
+            <!--header-right ends....-->
+        </div>
+        <!--container ends.... 	-->	
+    </header>
+    <!--header ends......-->
+    
+    
+    <section class="menu-area">
+    	<div class="container">
+        	<div class="nav dnt3-menu">
+                            <ul class="dropdown clear ">
+                             <li class="active" ><a href="index.php">Home</a></li>				
+                             <li class="sub"><a href="#">Wat we doen</a>
+                                <ul>
+                                    	<li><a href="#">Chiptuning</a></li>
+                                        <li><a href="#">EGR uitschakelen</a></li>
+                                        <li><a href="#">4x4 vermogensmeting</a></li>
+                                        <li><a href="#">Software ontwikkeling</a></li>   
+                                        <li><a href="#">Foutcode uitschakelen</a></li>  
+                                </ul>                    
+                            </li>
+                                <li class="sub"><a href="#">Chiptuning </a>
+                                  <ul>
+                                        <li><a href="../micro-loan">Eco Chiptuning</a></li>
+                                        <li><a href="../peer-peer">ATM-Chiptuning</a></li>
+                                     </ul>   
+                                </li>
+                                <li><a href="../page.php" >Diensten</a></li>
+                                <li><a href="#">Op Locatie</a></li>
+                                <li><a href="#">Leaseauto</a></li>
+                                <li><a href="#">Offerte</a></li>
+                                <li><a href="#">Contact</a></li>
+                            </ul>
+           </div>
+           <!--dnt3 ends....-->
+        
+        </div>
+        <!--container ends....	-->
+    </section>
+    <!--menu-area ends....-->
+    
+    
+    <section class="banner-area">
+    	<div class="container pos">
+            <ul class="bxslider">
+                      <li>
+                            <img src="../images/banner1.jpg" alt="banner1">
+                      </li>
+                      <li>
+                            <img src="../images/banner2.jpg" alt="banner1">
+                      </li>
+                      <li>
+                            <img src="../images/banner3.jpg" alt="banner1">
+                      </li>
+            </ul>
+            
+            
+            <!--banner form starts.....-->
+            <div class="total-form">
+                <form role="form">
+                         <div class="form-group">
+                              <select class="form-control" id="sel1">
+                                <option>Kies uw merk</option>
+                                <option>Abarth</option>
+                                <option>Alfa Romeo</option>
+                                <option>Alpina</option>
+                              </select>
+                         </div>
+                         <div class="form-group">
+                              <select class="form-control" id="sel1">
+                                <option>Kies uw model</option>
+                              </select>
+                         </div>
+                         <div class="form-group">
+                              <select class="form-control" id="sel1">
+                                <option>Kies de generatie</option>
+                              </select>
+                         </div>
+                         <div class="form-group">
+                              <select class="form-control" id="sel1">
+                                <option>Kies het motortype</option>
+                              </select>
+                         </div>
+                         <input type="submit" class="btn btn-default" value="Info">
+                         <div class="form-sub">
+                         	<a href="#">Offerte</a>
+                         </div>
+                </form>
+            </div>
+            <!--banner form ends.....-->
+        </div>								
+    </section>
+    <!--banner-area ends.......-->
+    
+    
+    <section class="about">
+    	<div class="container">
+        	<div class="section-title bg1">
+        		<h1>About Company</h1>
+            </div>
+            <!--section-title ends.....-->
+            
+        	<div class="col-md-12 col-sm-12 bg1">
+            	<div class="single-about bg1">
+                    <p>Autofuture is gespecialiseerd in prestatie- en vermogensoptimalisatie van auto’s. Dankzij onze hoogwaardige apparatuur zijn wij in staat om de nieuwste modellen te voorzien van chiptuning. Autofuture Tuning is een betrouwbaar bedrijf waar service en kwaliteit voorop staan. Wij profileren ons door onze diensten tegen scherpe prijzen te leveren, in combinatie met hoogwaardige service. Uiteraard is chiptuning op locatie mogelijk bij Autofuture, hiervoor kunt u direct een afspraak maken, Wij zullen met onze speciaal ingerichtte werkbus bij u op locatie langskomen om uw auto te voorzien van Chiptuning.</p>	
+                </div>
+                <!--single-business ends....-->	
+            </div>	
+            <!--col-md-4 col-sm-4 ends...-->
+            
+            
+        </div>
+        <!-- container ends....-->	
+    </section>
+     <!--section  about ends.......-->
+     
+     <section class="about2">
+    	<div class="container">
+            <div class="col-md-6 col-sm-6">
+            	<div class="single-about2">
+                    <h2>Cost Savings</h2>
+                    <p>Gezien elk auto uniek is vergt er een specifieke software-instellingen toegepast te worden bij uw auto. Daarom wordt iedere auto op professionele en unieke wijze gechiptuned, waarbij de software zo optimaal mogelijk word aangepast uiteraard wel binnen de veiligheidsmarge’s van de motor.de vermogenstoename is in veel gevallen rond de 30% van uw huidige vermogen, dit word specifiek opmaat gemaakt en uiteraard zo veilig en gezond mogelijk voor uw motor aangepast. Uiteraard is het mogelijk om meer vermogen uit uw auto te halen. Naast een vlottere en meer comfortabele rijstijl levert chiptuning eveneens brandstofbesparing op. Dankzij onze softwareparameters kan er een besparing van meer dan 5% worden gerealiseerd.</p>
+                   <div class="read">
+                   		<a href="#">Read More</a>
+                   </div>
+                </div>
+                <!--single-business ends....-->	
+            </div>	
+            <!--col-md-6 col-sm-6 ends...-->
+            
+            <div class="col-md-6 col-sm-6">
+            	<div class="single-about2">
+                    <h2>WAAROM AUTOFUTURE ?</h2>
+                    <ul>
+                    	<li><i class="fa fa-check-square-o" aria-hidden="true"></i>vinkje  Óók in het Weekend</li>
+                    	<li><i class="fa fa-check-square-o" aria-hidden="true"></i>vinkje  Óók in Avonduren</li>
+                    	<li><i class="fa fa-check-square-o" aria-hidden="true"></i>vinkje  Op maat geschreven tuning</li>
+                    	<li><i class="fa fa-check-square-o" aria-hidden="true"></i>vinkje  De scherpste prijs</li>
+                    	<li><i class="fa fa-check-square-o" aria-hidden="true"></i>vinkje  Ook ECO-tuning</li>
+                    	<li><i class="fa fa-check-square-o" aria-hidden="true"></i>vinkje  Software garantie</li>
+                    	<li><i class="fa fa-check-square-o" aria-hidden="true"></i>vinkje  Klaar terwijl u wacht</li>
+                    	<li><i class="fa fa-check-square-o" aria-hidden="true"></i>vinkje  Meer dan 5 Jaar Ervaring</li>
+                    	<li><i class="fa fa-check-square-o" aria-hidden="true"></i>vinkje  Chiptuning op Locatie</li>
+                    </ul>	
+                </div>
+                <!--single-business ends....-->	
+            </div>	
+            <!--col-md-4 col-sm-4 ends...-->
+            
+            
+        </div>
+        <!-- container ends....-->	
+    </section>
+     <!--section  about2 ends.......-->
+     
+     <footer>
+     	<div class="container">
+        	<div class="col-md-3 col-sm-3">
+            	<div class="single-footer">
+                	<h3>Contact Us</h3>
+                	<ul>
+                		<li>AutoFuture Chiptuning</li>
+                		<li>Ariaplein 33-21</li>
+                		<li>3816EH Amersfoort</li>
+                		<li>06 85 09 72 73</li>
+                        <li>info@AutoFuture.nl</li>
+                	</ul>
+                </div>
+                <!--single-footer ends....-->
+            </div>
+           <!-- col-md-3 ends.....-->
+           
+           <div class="col-md-3 col-sm-3">
+            	<div class="single-footer">
+                	<h3>Openingstijde</h3>
+                	<ul>
+                		<li>Ma t/m Vr 09:00 T/M 18:00</li>
+                        <li>Za 09:00 T/M 17:00</li>
+                        <li>Graag bellen voordat u langskomt</li>
+                        <li>Wij werken ook op locatie !</li>
+                        <li>06-85097273</li>
+                	</ul>
+                </div>
+                <!--single-footer ends....-->
+            </div>
+           <!-- col-md-3 ends.....-->
+           
+           <div class="col-md-3 col-sm-3">
+            	<div class="single-footer">
+                	<h3>Quick Link</h3>
+                	<ul>
+                		<li><a href="#">Chiptuning Amersfoort</a></li>
+                        <li><a href="#">Chiptuning Utrecht</a></li>
+                        <li><a href="#">Chiptuning Almere</a></li>
+                        <li><a href="#">Chiptuning Nijmegen</a></li>
+                        <li><a href="#">Chiptuning Middelburg</a></li>
+                	</ul>
+                </div>
+                <!--single-footer ends....-->
+            </div>
+           <!-- col-md-3 ends.....-->
+           
+           <div class="col-md-3 col-sm-3">
+            	<div class="single-footer">
+                	<h3>Get In Touch</h3>
+                	<ul>
+                		<li><i class="fa fa-skype" aria-hidden="true"></i><b>24/7 WHATSAPP SERVICE</b></li>
+                		<li>Whatsapp Service 06 - 85 09 72 73</li>
+                		<li>Info@autofuture.nl</li>
+                	</ul>
+                </div>
+                <!--single-footer ends....-->
+            </div>
+           <!-- col-md-3 ends.....-->
+           
+        </div>
+        <!--container ends....-->
+     </footer>
+     <!--footer ends....-->
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+                               
+                               
+                              
+    
+   
+            
+     
+                     
+     
+    
+
+
+
+
+
+<!-- **** JS(Java Script) FILES****** --> 
+<!-- Lib -->
+<script type="text/javascript" src="../js/jquery-1.10.1.min.js"></script>
+<!-- BOOTSTRAP js -->
+<script type="text/javascript" src="../js/bootstrap.js"></script>
+
+<!-- bx JS -->
+<script type="text/javascript" src="../js/jquery.bxslider.js"></script>
+<script type="text/javascript">
+	$(document).ready(function(){
+	  $('.bxslider').bxSlider();
+	});
+</script>
+
+<!--modal------->
+<script>
+$(document).ready(function(){
+    $("#myBtn").click(function(){
+        $("#myModal").modal();
+    });
+	$("#myBtn2").click(function(){
+        $("#myModal2").modal();
+    });
+	$("#myBtn3").click(function(){
+        $("#myModal3").modal();
+    });
+});
+</script>
+
+<!--accordion.....-->
+<script src="../js/jquery.contenttoggle.js"></script>
+<script src="../js/main.js"></script>
+
+<!-- Menu JS -->
+<script type="text/javascript" src="../js/menu-responsive.js"></script>
+
+<!--uikit.....-->
+<script type="text/javascript" src="../js/uikit.js"></script>
+
+
+
+
+</body>
+</html>
